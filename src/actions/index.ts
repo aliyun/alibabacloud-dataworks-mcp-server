73fd91d87b18c05f13dfea8cf766b17127c7eb45
samuelhsin <caramels@gmail.com>
2025-04-23 15:13:56 +0800

    Delete src directory

--- src/actions/index.ts
+++
@@ -1,8 +0,0 @@
-
-import listProjectsAction from "./agent/listProjects.js";
-
-export const ACTIONS = {
-  LIST_PROJECTS_ACTION: listProjectsAction,
-};
-
-export type { ActionTool, ActionExample, Handler } from "../types/action.js";
